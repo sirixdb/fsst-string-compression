b9ff6614010ee411eace9f49498bf00e58362f2f
root <root@DESKTOP-80FDR9T.localdomain>
2023-08-17 15:11:47 -0400

    Scaffolding Encoder

---
+++ lib/src/main/java/Notes.md
@@ -0,0 +1,6 @@
+https://github.com/cwida/fsst/tree/master 
+Sanity folder - minimal code to covert from. More at the original repo
+
+#Technical Java Notes
+1. C/C++ char 1 byte
+2. Java char 2 bytes.
--- lib/src/main/java/fsst/SymbolTable.java
+++ lib/src/main/java/fsst/SymbolTable.java
@@ -337,4 +337,59 @@ public class SymbolTable {
         // https://github.com/cwida/fsst/blob/42850e13ba220dbba5fd721a4c54f969e2a45ac5/libfsst.cpp#L160
         return best;
     }
+
+    // TODO:type interference
+    void makeTable(SymbolTable symbolTable, Counters counters, int sampleFrac) {
+        //Hashset (needed because we can generate duplicate candidates)
+        HashSet <QSymbol> candidates; //they used unsigned set
+
+    //TODO: Google shift optimization over division. Caveats: auto done by compiler, dealing with negative
+    }
+
+    // add candidate symbols based on counted frequency
+      for (u32 pos1=0; pos1<FSST_CODE_BASE+(size_t) st->nSymbols; pos1++) { 
+         u32 cnt1 = counters.count1GetNext(pos1); // may advance pos1!!
+         if (!cnt1) continue;
+
+         // heuristic: promoting single-byte symbols (*8) helps reduce exception rates and increases [de]compression speed
+         Symbol s1 = st->symbols[pos1];
+         addOrInc(cands, s1, ((s1.length()==1)?8LL:1LL)*cnt1); //third one should be
+         //Long.parseUnsignedLong
+
+         if (sampleFrac >= 128 || // last round we do not create new (combined) symbols
+             s1.length() == Symbol::maxLength || // symbol cannot be extended
+             s1.val.str[0] == st->terminator) { // multi-byte symbols cannot contain the terminator byte
+            continue;
+         }
+         for (u32 pos2=0; pos2<FSST_CODE_BASE+(size_t)st->nSymbols; pos2++) { 
+            u32 cnt2 = counters.count2GetNext(pos1, pos2); // may advance pos2!!
+            if (!cnt2) continue;
+
+            // create a new symbol
+            Symbol s2 = st->symbols[pos2];
+            Symbol s3 = concat(s1, s2);
+            if (s2.val.str[0] != st->terminator) // multi-byte symbols cannot contain the terminator byte
+               addOrInc(cands, s3, cnt2);
+         }
+      }
+
+    /**
+     * Helper function.
+     * 
+     * @param count unsigned 64byte must be parsed to long
+     */
+    void addOrInc(HashMap <QSymbol> candidates, Symbol s, Long count, int sampleFrac){
+        if (count < (5*sampleFrac)/128) return; // Improves both compression speed (less candidates), but also quality!!
+        
+        QSymbol q;
+        q.symbol = s;
+        q.gain = count * s.length();
+         
+         var it = candidates.find(q);
+         if (it != ccandidates.end()) {
+            q.gain += (*it).gain;
+            candidates.erase(*it);
+         }
+         candidates.insert(q);
+    }
 }
